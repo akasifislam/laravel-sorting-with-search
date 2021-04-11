79d721bea8806da18396e8a0ed3eedc409f79084
akasifislam <aseasifislam@gmail.com>
2021-04-11 22:54:06 +0600

    controller created and route define

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,4 +11,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+
+Route::group(['as' => 'app.', 'prefix' => 'app', 'middleware' => ['auth']], function () {
+    Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('/posts/store', [PostController::class, 'store'])->name('post.store');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
+    Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->name('post.edit');
+    Route::put('/posts/{id}/update', [PostController::class, 'update'])->name('post.update');
+    Route::delete('/posts/{id}/destroy', [PostController::class, 'destroy'])->name('post.destroy');
+});
